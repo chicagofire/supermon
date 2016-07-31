696c4d604271f2b4068c7f42a3dadeeee0d67b74
chicagofire <jeremy.mayes@gmail.com>
2016-07-31 00:02:47 -0500

    add an entry point for cron

--- setup.py
+++ setup.py
@@ -8,16 +8,20 @@ version_txt = os.path.join(here, 'supermon/version.txt')
 version = open(version_txt).read().strip()
 
 dist = setup(
-        name='supermon',
-        version=version,
-        license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
+    name='supermon',
+    version=version,
+    license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
     url='https://github.com/chicagofire/supermon',
-        description='A set up supervisor utilities',
-        author='Jeremy Mayes',
-        author_email='jeremy.mayes@gmail.com',
-        packages=find_packages(),
-        install_requires=requires,
-        include_package_data=True,
-        zip_safe=False
+    description='A set up supervisor utilities',
+    author='Jeremy Mayes',
+    author_email='jeremy.mayes@gmail.com',
+    packages=find_packages(),
+    install_requires=requires,
+    include_package_data=True,
+    zip_safe=False,
+    entry_points = """\
+        [console_scripts]
+        supervisor_cron = supermon.cron:main
+        """
 )
 
